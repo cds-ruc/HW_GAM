32d9d2913cd22e4e9c1ffd4aaea15b79ac288737
lmjhh <576788582@qq.com>
2023-07-12 18:00:08 +0800

    change cmakelist

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,22 @@
 #include "mcs/rpc/server.h"
 #include "mcs/rpc/client.h"
 
-void plus(int a, int b) {
-  // printf("run plus\n");
-  while(1) {
-    std::cout << "plus " << a << " + " << b << " = " <<  a + b << std::endl;
-  }
-}
-MCS_REMOTE(plus);
+//GAM
+#include "structure.h"
+// #include "worker.h"
+#include "settings.h"
+#include "worker_handle.h"
+// #include "master.h"
+// #include "gallocator.h"
+// #include "workrequest.h"
+
+// void plus(int a, int b) {
+//   // printf("run plus\n");
+//   while(1) {
+//     std::cout << "plus " << a << " + " << b << " = " <<  a + b << std::endl;
+//   }
+// }
+// MCS_REMOTE(plus);
 
 // int main() {
 //   mcs::Init();
@@ -33,36 +42,36 @@ DEFINE_string(workType, "worker", "有master/wroker两种, 默认工作类型是
 DEFINE_int32(port, 33068, "Server 的监听端口, 0(默认) 随机端口");
 DEFINE_string(host, "127.0.0.1", "server ip");
 
-std::unique_ptr<mcs::rpc::McsServer> rpcservice;
-static photon::net::EndPoint ep;
+// std::unique_ptr<mcs::rpc::McsServer> rpcservice;
+// static photon::net::EndPoint ep;
 
-void handle_null(int) {}
-void handle_term(int) { rpcservice.reset(); }
+// void handle_null(int) {}
+// void handle_term(int) { rpcservice.reset(); }
 
 int main(int argc, char** argv) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-    photon::init();
-    DEFER(photon::fini());
-    photon::sync_signal(SIGPIPE, &handle_null);
-    photon::sync_signal(SIGTERM, &handle_term);
-    photon::sync_signal(SIGINT, &handle_term);
+    // photon::init();
+    // DEFER(photon::fini());
+    // photon::sync_signal(SIGPIPE, &handle_null);
+    // photon::sync_signal(SIGTERM, &handle_term);
+    // photon::sync_signal(SIGINT, &handle_term);
 
     if (FLAGS_workType.c_str()[0] != 'w') {
 
       // start server
       // construct rpcservice
-      rpcservice.reset(new mcs::rpc::McsServer());
-      rpcservice->run(FLAGS_port);
+      // rpcservice.reset(new mcs::rpc::McsServer());
+      // rpcservice->run(FLAGS_port);
     } else {
       // mcs::rpc::McsClient client;
       // ep = photon::net::EndPoint(photon::net::IPAddr(FLAGS_host.c_str()),
       //                           FLAGS_port);
       // client.rpc_resister(ep);
-      mcs::Init();
-      for (int i = 0; i < 3; i++) {
-        mcs::Task(plus).Remote(i,i);
-      }
+      // mcs::Init();
+      // for (int i = 0; i < 3; i++) {
+      //   mcs::Task(plus).Remote(i,i);
+      // }
       // for (int i = 0; i < 3; i++) {
       //   mcs::Get(results[i]);
       // }
